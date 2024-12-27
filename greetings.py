4463b80816dd14265f3f9e4958af709dacf69977
Fugaxiiii <RJT_Y@mail.ru>
2024-12-27 16:43:41 +0300

    Greetings

---
+++ greetings.py
@@ -0,0 +1,12 @@
+username='Fugaxiiii'
+title='greetings'
+content='practice_of_using_variables'
+status='in_progress'
+created_date='27.12.24'
+issue_date='27.01.25'
+print("Имя пользователя:", username)
+print("Заголовок заметки", title)
+print("Описание заметки", content)
+print("Статус заметки", status)
+print("Дата создания заметки", created_date)
+print("Дата истечения заметки", issue_date)
