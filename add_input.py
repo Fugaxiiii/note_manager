25f75520ea785f5a59ef2f19bcb04798f72c12d6
Fugaxiiii <RJT_Y@mail.ru>
2024-12-28 16:54:35 +0300

    Заметки

---
+++ add_input.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+created_date_input = input('Введите дату создания заметки ')
+issue_date_input = input ('Введите дату изменения заметки ')
+
+created_date = datetime.strptime(created_date_input, "%d.%m.%y")
+issue_date = datetime.strptime(issue_date_input, "%d.%m.%y")
+
+temp_created_date = created_date.strftime("%d.%m")
+temp_issue_date = issue_date.strftime("%d.%m")
+
+title=input('Введите заголовок заметки ')
+
+content=input('Введите содержание заметки ')
+
+print("\nИнформация о заметке:")
+print(f"Заголовок: {title}")
+print(f"Содержание: {content}")
+print(f"Дата создания: {temp_created_date}")
+print(f"Дата изменения: {temp_issue_date}")
