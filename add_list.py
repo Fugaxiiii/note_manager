638ee5bdec880673b07c51b8fa6a6e0af8029c37
Fugaxiiii <RJT_Y@mail.ru>
2025-01-04 19:01:00 +0300

    Список

---
+++ add_list.py
@@ -0,0 +1,10 @@
+main_title=input('Основной заголовок ')
+add_title=input('Подзаголовок ')
+com=input('Комментарий ')
+
+titles_list=[main_title,add_title, com]
+
+print(titles_list)
+print('Основной заголовок:', main_title)
+print('Подзаголовок:', add_title)
+print('Комментарий:', com)
